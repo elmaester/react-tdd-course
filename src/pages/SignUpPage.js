adcca6592e47b90e689224cde23dc37a2af10a08
elmaester <olegmorket@gmail.com>
2022-06-24 14:31:03 +0300

    apply styling to SignUp form

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -13,43 +13,61 @@ const SignUpPage = () => {
   };
   return (
     <>
-      <form>
-        <h1>Sign Up</h1>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="text"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <label htmlFor="repeat-password">Repeat password</label>
-        <input
-          type="password"
-          id="repeat-password"
-          value={repeatPassword}
-          onChange={(e) => setRepeatPassword(e.target.value)}
-        />
-        <button
-          type="submit"
-          disabled={!passwordsAreValidAndEqual(password, repeatPassword)}
-          onClick={submit}
-        >
-          Sign Up
-        </button>
+      <form className="SignUp__form">
+        <div className="SignUp__title">
+          <h1>Sign Up</h1>
+        </div>
+
+        <div className="SignUp__body">
+          <div className="SignUp__field">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="SignUp__field">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="SignUp__field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="SignUp__field">
+            <label htmlFor="repeat-password">Repeat password</label>
+            <input
+              type="password"
+              id="repeat-password"
+              value={repeatPassword}
+              onChange={(e) => setRepeatPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="SignUp__button">
+          <button
+            type="submit"
+            disabled={!passwordsAreValidAndEqual(password, repeatPassword)}
+            onClick={submit}
+          >
+            Sign Up
+          </button>
+        </div>
       </form>
     </>
   );
